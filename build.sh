9bda90a73bec6556e4ba8cd1ad6c4f4dc9ee4e13
Nicolas Anjoran <nicolas.anjoran@gmail.com>
2023-08-16 19:07:25 +0200

    added build.sh and releases

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BINARY_NAME=cronwrap
+RELEASE_DIR=release
+
+mkdir -p $RELEASE_DIR
+
+echo "Building for Linux..."
+GOOS=linux GOARCH=amd64 go build -o $RELEASE_DIR/${BINARY_NAME}_linux_amd64
+GOOS=linux GOARCH=arm go build -o $RELEASE_DIR/${BINARY_NAME}_linux_arm
+
+echo "Building for macOS..."
+GOOS=darwin GOARCH=amd64 go build -o $RELEASE_DIR/${BINARY_NAME}_macos_intel
+GOOS=darwin GOARCH=arm64 go build -o $RELEASE_DIR/${BINARY_NAME}_macos_arm64
+
+echo "Build complete!"
