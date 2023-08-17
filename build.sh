d62328fde93056ff1b8ce1cf4b5d8b437a2a38ae
Nicolas Anjoran <nicolas.anjoran@gmail.com>
2023-08-17 08:47:20 +0200

    changed mac binary name

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ GOOS=linux GOARCH=amd64 go build -o $RELEASE_DIR/${BINARY_NAME}_linux_amd64
 GOOS=linux GOARCH=arm go build -o $RELEASE_DIR/${BINARY_NAME}_linux_arm
 
 echo "Building for macOS..."
-GOOS=darwin GOARCH=amd64 go build -o $RELEASE_DIR/${BINARY_NAME}_macos_intel
+GOOS=darwin GOARCH=amd64 go build -o $RELEASE_DIR/${BINARY_NAME}_macos_amd64
 GOOS=darwin GOARCH=arm64 go build -o $RELEASE_DIR/${BINARY_NAME}_macos_arm64
 
 echo "Build complete!"
